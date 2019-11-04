0a603bccfeb7fe53221a4465eff995efc9a203ab
Amir Ben Artzi <abenartzi@gmail.com>
2019-11-04 22:24:46 +0200

    FoodPicker done without desert

--- src/FoodPicker/FoodPicker.js
+++ src/FoodPicker/FoodPicker.js
@@ -6,17 +6,40 @@ import React, { Component } from 'react';
  */
 
 class FoodPicker extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			nameContent: '',
+			mealContent: '',
+			value:'Frangelico'
+
+
+		};
+	}
+	nameTypeHandler(event) {
+		this.setState({nameContent:event.target.value})
+	}
+
+	optionHandler(event) {
+		this.setState({value:event.target.value})
+	}
+
+	mealTypeHandler(event) {
+		this.setState({mealContent:event.target.value})
+	}
+
+
 	render() {
 		return (
 			<div>
 				<h1>Order special meal:</h1>
 				<div>
 					Your name:
-					<input type="text" />
+					<input type="text" onChange={this.nameTypeHandler.bind(this)}/>
 				</div>
 				<div>
 					Choose restaurant:
-					<select>
+					<select onChange={this.optionHandler.bind(this)}>
 						<option value="Frangelico">Frangelico</option>
 						<option value="Mina Tomei">Mina Tomei</option>
 						<option value="Rak Basar">Rak Basar</option>
@@ -25,7 +48,7 @@ class FoodPicker extends Component {
 				</div>
 				<div>
 					Favorite meal:
-					<input type="text" />
+					<input type="text" onChange={this.mealTypeHandler.bind(this)}/>
 				</div>
 				<div>
 					Want a desert?
@@ -34,9 +57,9 @@ class FoodPicker extends Component {
 
 				<div>
 					<h2>Your reservation:</h2>
-					Hi [NAME]! <br />
-					We are glad you want to reserve a table at [RESTAURANT].<br />
-					We will make sure that your favorite meal, [MEAL] is available.<br />
+					Hi {this.state.nameContent}! <br />
+					We are glad you want to reserve a table at {this.state.value}.<br />
+					We will make sure that your favorite meal, {this.state.mealContent} is available.<br />
 					Additionally, our chef will make a special desert for you!<br />
 				</div>
 			</div>
